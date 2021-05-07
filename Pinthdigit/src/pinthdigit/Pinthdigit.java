7f5d0e7564066de86a1e5c62234400ae7d18a290
Anjolaoluwa Olutomilayo 17CJ022536 <anjolaolutomilayo@gmail.com>
2021-05-07 15:38:31 +0100

    Add files via upload

--- Pinthdigit/src/pinthdigit/Pinthdigit.java
+++ Pinthdigit/src/pinthdigit/Pinthdigit.java
@@ -8,21 +8,21 @@ import static java.lang.Math.PI;
 import java.math.BigDecimal;
 import java.util.Scanner;
 /**
- *
- * @author ariel
+ *This program computes the value of PI to n decimal places
+ * @author Anjolaoluwa
  */
 public class Pinthdigit {
 public static void nthdigit(){
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the number of decimal places (between 0 and 10): ");
-        int i = input.nextInt();
+        int n = input.nextInt();
         
-        if (i>10 || i<0){
+        if (n>10 || n<0){
             System.out.println("Excess Decimal Places!");
 }
         else{
-            BigDecimal round=new BigDecimal(PI).setScale​(i, BigDecimal.ROUND_DOWN);
-            System.out.println("Your value of PI is "+ round + " to " + i + " decimal places.");
+            BigDecimal round=new BigDecimal(PI).setScale​(n, BigDecimal.ROUND_DOWN);
+            System.out.println("Your value of PI is "+ round + " to " + n + " decimal places.");
             
 }
    
@@ -32,8 +32,7 @@ public static void nthdigit(){
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Pinthdigit Anjie=new Pinthdigit();
-        Anjie.nthdigit();
+        Pinthdigit.nthdigit();
        
     }
     
